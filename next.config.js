a037d90e3b3313beeec465e14b169927bcc470e6
dkile <dkile532@naver.com>
2023-03-27 22:23:10 +0900

    Add security header option to next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
 const path = require("path");
 
+const ContentSecurityPolicy = `
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
